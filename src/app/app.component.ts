e16a268000b1628383df2f98bcd367be5f456c28
Sonam <66429024+sonam2294@users.noreply.github.com>
2020-06-10 15:46:56 +0530

    updated time difference function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -106,7 +106,7 @@ export class AppComponent {
    */
   getTimeDiff(date1: Date, date2: Date): number {
     let difference_In_Time = date2.getTime() - date1.getTime();
-    return Math.round(difference_In_Time / (1000 * 3600 * 24)) + 1;
+    return Math.ceil(difference_In_Time / (1000 * 3600 * 24));
   }
 
   /**
